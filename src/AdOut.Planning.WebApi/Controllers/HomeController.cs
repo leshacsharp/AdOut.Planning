f2d761b996511548cc52f55c5990984b44fbf791
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2020-02-25 13:15:20 +0300

    init structure of project

---
+++ src/AdOut.Planning.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AdOut.Planning.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world and AdOut.Planning, good luck (-:");
+        }
+    }
+}
