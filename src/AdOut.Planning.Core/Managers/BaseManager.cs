a44288e75ba14de3536f0c6b77999b8a5a6a6fed
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2020-02-25 13:36:49 +0300

    Created abstraction for managers

---
+++ src/AdOut.Planning.Core/Managers/BaseManager.cs
@@ -0,0 +1,48 @@
+﻿using AdOut.Planning.Model.Interfaces.Managers;
+using AdOut.Planning.Model.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AdOut.Planning.Core.Managers
+{
+    public abstract class BaseManager<TEntity> : IBaseManager<TEntity> where TEntity : class
+    {
+        private readonly IBaseRepository<TEntity> _repository;
+        public BaseManager(IBaseRepository<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Create(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _repository.Create(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _repository.Delete(entity);
+        }
+
+        public Task<List<TEntity>> ReadAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _repository.Read(predicate).ToListAsync();
+        }
+
+        public void Update(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _repository.Update(entity);
+        }
+    }
+}
---
+++ src/AdOut.Planning.Model/Interfaces/Managers/IBaseManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AdOut.Planning.Model.Interfaces.Managers
+{
+    public interface IBaseManager<TEntity> where TEntity : class
+    {
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<List<TEntity>> ReadAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
