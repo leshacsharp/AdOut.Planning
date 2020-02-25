8c46cfb848f1e8d7ee2b4c90f2595eba218fbc89
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2020-02-25 13:36:07 +0300

    Created database context

---
+++ src/AdOut.Planning.DataProvider/Context/PlanningContext.cs
@@ -0,0 +1,14 @@
+﻿using AdOut.Planning.Model.Interfaces.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdOut.Planning.DataProvider.Context
+{
+    public class PlanningContext : DbContext, IDatabaseContext
+    {
+        public PlanningContext(DbContextOptions<PlanningContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+    }
+}
---
+++ src/AdOut.Planning.Model/Interfaces/Context/IDatabaseContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdOut.Planning.Model.Interfaces.Context
+{
+    public interface IDatabaseContext
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+    }
+}
