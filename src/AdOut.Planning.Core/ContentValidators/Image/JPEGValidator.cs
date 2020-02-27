17cc37c5eab0eacdb61b5f6a33ec1b3a0acbea7d
Starchenko Aleksey <A.Starchanka@sam-solutions.com>
2020-02-27 15:38:28 +0300

    Added JPEG and PNG content validators

---
+++ src/AdOut.Planning.Core/ContentValidators/Image/JPEGValidator.cs
@@ -0,0 +1,14 @@
+﻿using AdOut.Planning.Model;
+using AdOut.Planning.Model.Interfaces.Repositories;
+
+namespace AdOut.Planning.Core.ContentValidators.Image
+{
+    public class JPEGValidator : ImageBaseValidator
+    {
+        public JPEGValidator(IConfigurationRepository configurationRepository)
+            : base(configurationRepository, Constants.ContentSignature.JPEG)
+        {
+
+        }
+    }
+}
---
+++ src/AdOut.Planning.Core/ContentValidators/Image/PNGValidator.cs
@@ -0,0 +1,13 @@
+﻿using AdOut.Planning.Model;
+using AdOut.Planning.Model.Interfaces.Repositories;
+
+namespace AdOut.Planning.Core.ContentValidators.Image
+{
+    public class PNGValidator : ImageBaseValidator
+    {
+        public PNGValidator(IConfigurationRepository configurationRepository)
+            : base(configurationRepository, Constants.ContentSignature.PNG)
+        {
+        }
+    }
+}
