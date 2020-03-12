e049f5980e1310d134eef34e72ff8193a6ce5e16
Starchenko Aleksey <A.Starchanka@sam-solutions.com>
2020-03-12 18:18:07 +0300

    Created AdPointTime schedule validator

--- src/AdOut.Planning.Core/ScheduleValidators/AdPointTimeValidator.cs
+++ src/AdOut.Planning.Core/ScheduleValidators/AdPointTimeValidator.cs
@@ -1,7 +1,7 @@
 ﻿using AdOut.Planning.Model.Classes;
 using AdOut.Planning.Model.Interfaces.Schedule;
 using System;
-using System.Threading.Tasks;
+using static AdOut.Planning.Model.Constants;
 
 namespace AdOut.Planning.Core.ScheduleValidators
 {
@@ -9,12 +9,24 @@ namespace AdOut.Planning.Core.ScheduleValidators
     {
         private IScheduleValidator _nextValidator;
 
-        public Task ValidAsync(ScheduleValidationContext context)
+        public void Valid(ScheduleValidationContext context)
         {
             if (context == null)
                 throw new ArgumentNullException(nameof(context));
 
-            return _nextValidator?.ValidAsync(context);
+            foreach (var adPoint in context.AdPointValidations)
+            {
+                if(context.StartTime < adPoint.StartWorkingTime || context.EndTime > adPoint.EndWorkingTime)
+                {
+                    var schedulerTimeMode = $"{context.StartTime} - {context.EndTime}";
+                    var adPointTimeMode = $"{adPoint.StartWorkingTime} - {adPoint.EndWorkingTime}";
+
+                    var validationMessage = string.Format(ScheduleValidationMessages.ScheduleTimeIsNotAllowed_T, schedulerTimeMode, adPoint.Location, adPointTimeMode);
+                    context.Errors.Add(validationMessage);
+                }
+            }
+
+            _nextValidator?.Valid(context);
         }
 
         public void SetNextValidator(IScheduleValidator validator)
