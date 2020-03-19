9c3f50350a62cd6d68ddfc3cb5e6f995e61ca9bc
Starchenko Aleksey <A.Starchanka@sam-solutions.com>
2020-03-19 20:35:09 +0300

    Refactoring AdPointRepository

--- src/AdOut.Planning.DataProvider/Repositories/AdPointRepository.cs
+++ src/AdOut.Planning.DataProvider/Repositories/AdPointRepository.cs
@@ -17,57 +17,76 @@ namespace AdOut.Planning.DataProvider.Repositories
         {
         }
 
-        public Task<List<AdPointValidation>> GetAdPointsValidationAsync(int[] adPointIds, DateTime planStart, DateTime planEnd)
+        public async Task<List<AdPointValidation>> GetAdPointsValidationAsync(int[] adPointIds, DateTime planStart, DateTime planEnd)
         {
-            var query = from ap in Context.AdPoints
-                        where adPointIds.Contains(ap.Id)
+            var query = from ap in Context.AdPoints.Where(ap => adPointIds.Contains(ap.Id))
 
-                        select new AdPointValidation()
+                        join pap in Context.PlanAdPoints on ap.Id equals pap.AdPointId into planAdPointsJoin
+                        from pap in planAdPointsJoin.DefaultIfEmpty()
+
+                        join p in Context.Plans on pap.PlanId equals p.Id into plansJoin
+                        from p in plansJoin.DefaultIfEmpty()
+
+                        join apd in Context.AdPointDaysOff on ap.Id equals apd.AdPointId into adPointDaysOffJoin
+                        from apd in adPointDaysOffJoin.DefaultIfEmpty()
+
+                        join d in Context.DaysOff on apd.DayOffId equals d.Id into daysOffJoin
+                        from d in daysOffJoin.DefaultIfEmpty()
+
+                        where p.StartDateTime <= planStart && p.EndDateTime >= planStart ||
+                              p.StartDateTime <= planEnd && p.EndDateTime >= planEnd ||
+                              p.StartDateTime >= planStart && p.EndDateTime <= planEnd
+
+                        select new
                         {
-                            Location = ap.Location,
-                            StartWorkingTime = ap.StartWorkingTime,
-                            EndWorkingTime = ap.EndWorkingTime,
+                            ap.Id,
+                            ap.Location,
+                            ap.StartWorkingTime,
+                            ap.EndWorkingTime,
 
-                            Plans = from pap in ap.PlanAdPoints
-                                    join p in Context.Plans on pap.PlanId equals p.Id
-                                    where p.StartDateTime <= planStart && p.EndDateTime >= planStart || 
-                                          p.StartDateTime <= planEnd && p.EndDateTime >= planEnd || 
-                                          p.StartDateTime >= planStart && p.EndDateTime <= planEnd     
+                            DaysOff = d != null ? d.DayOfWeek : (DayOfWeek?)null,
 
-                                    orderby p.StartDateTime
+                            Plan = p != null ? new PlanValidation()
+                            {
+                                Type = p.Type,
+                                StartDateTime = p.StartDateTime,
+                                EndDateTime = p.EndDateTime,
 
-                                    select new PlanValidation
-                                    {
-                                        Type = p.Type,
-                                        StartDateTime = p.StartDateTime,
-                                        EndDateTime = p.EndDateTime,
+                                Schedules = p.Schedules.Where(s => s.Date == null || s.Date <= planEnd).Select(s => new ScheduleValidation()
+                                {
+                                    StartTime = s.StartTime,
+                                    EndTime = s.EndTime,
+                                    BreakTime = s.BreakTime,
+                                    DayOfWeek = s.DayOfWeek,
+                                    Date = s.Date
+                                }),
 
-                                        Schedules = from s in p.Schedules
-                                                    where s.Date == null || s.Date <= planEnd
-                                                   
-                                                    select new ScheduleValidation()
-                                                    {
-                                                        StartTime = s.StartTime,
-                                                        EndTime = s.EndTime,
-                                                        BreakTime = s.BreakTime,
-                                                        DayOfWeek = s.DayOfWeek,
-                                                        Date = s.Date
-                                                    },
-
-                                        PlanAds = from pa in p.PlanAds
-                                                  select new PlanAdValidation()
-                                                  {
-                                                      Order = pa.Order,
-                                                      TimePlayingSec = pa.TimePlayingSec
-                                                  }
-                                    },
-
-                            DaysOff = from apw in ap.AdPointDaysOff
-                                      join da in Context.DaysOff on apw.DayOffId equals da.Id
-                                      select da.DayOfWeek
+                                PlanAds = p.PlanAds.Select(pa => new PlanAdValidation()
+                                {
+                                    Order = pa.Order,
+                                    TimePlayingSec = pa.TimePlayingSec
+                                })
+                            } : null
                         };
 
-            return query.ToListAsync();
+            var adPointValidations = await query.ToListAsync();
+
+            var result = from apv in adPointValidations
+                      group apv by new { apv.Id, apv.Location, apv.StartWorkingTime, apv.EndWorkingTime }
+                      into apvGroup
+
+                      select new AdPointValidation()
+                      {
+                          Location = apvGroup.Key.Location,
+                          StartWorkingTime = apvGroup.Key.StartWorkingTime,
+                          EndWorkingTime = apvGroup.Key.EndWorkingTime,
+
+                          Plans = apvGroup.Where(apv => apv.Plan != null).Select(apv => apv.Plan),
+
+                          DaysOff = apvGroup.Where(apv => apv.DaysOff != null).Select(apv => apv.DaysOff.Value)
+                      };
+
+            return result.ToList();
         }
     }
 }
