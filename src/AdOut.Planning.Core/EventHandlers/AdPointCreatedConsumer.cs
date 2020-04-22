353aacf8dca8c887bc63dac4c76accc70b96372e
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2020-04-22 20:13:27 +0300

    AdPointCreated Consumer

---
+++ src/AdOut.Planning.Core/EventHandlers/AdPointCreatedConsumer.cs
@@ -0,0 +1,35 @@
+﻿using AdOut.Planning.Core.EventHandlers.Base;
+using AdOut.Planning.Model.Database;
+using AdOut.Planning.Model.Events;
+using AdOut.Planning.Model.Interfaces.Context;
+using AdOut.Planning.Model.Interfaces.Repositories;
+using AutoMapper;
+using System.Threading.Tasks;
+
+namespace AdOut.Planning.Core.EventHandlers
+{
+    public class AdPointCreatedConsumer : BaseConsumer<AdPointCreatedEvent>
+    {
+        private readonly IAdPointRepository _adPointRepository;
+        private readonly ICommitProvider _commitProvider;
+
+        public AdPointCreatedConsumer(
+            IAdPointRepository adPointRepository,
+            ICommitProvider commitProvider)
+        {
+            _adPointRepository = adPointRepository;
+            _commitProvider = commitProvider;
+        }
+
+        protected override Task HandleAsync(AdPointCreatedEvent deliveredEvent)
+        {
+            var mapperCfg = new MapperConfiguration(cfg => cfg.CreateMap(deliveredEvent.GetType(), typeof(AdPoint)));
+            var mapper = new Mapper(mapperCfg);
+
+            var adPoint = mapper.Map<AdPoint>(deliveredEvent);
+            _adPointRepository.Create(adPoint);
+
+            return _commitProvider.SaveChangesAsync();
+        }
+    }
+}
